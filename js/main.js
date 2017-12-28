6eebba81da495071db84b5fe5b2adbee769d27de
Maria Virginia <mariav53@gmail.com>
2017-12-28 18:09:08 +0100

    gulp update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,2 @@
-<<<<<<< HEAD
+
 'use strict';
-
-
-
-function shareSocial(){
-	console.log('hola');// document.getElementbyId('shareList').style.display ='block';
-}
-=======
-
->>>>>>> 5b5f89755c40977b41aee0a52301736534852b45
