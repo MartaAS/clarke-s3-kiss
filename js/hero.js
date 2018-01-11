d90c2153d8dd0601a7fd4d6caf6662e4225c1a71
Gala Calero <calero.vg@gmail.com>
2018-01-11 18:34:02 +0100

    media querys on header

---
+++ js/hero.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//para animar el hero cuando la pagina cargue
+
+// Variables
+var body = document.querySelector('body');
+var hero = document.querySelector('.block_header');
+
+
+// Functions
+function pageLoaded() {
+	hero.classList.add('page_load');
+};
+
+
+// Magic
+body.addEventListener('load', pageLoaded);
