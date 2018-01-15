773bf619060f0abefe3db804bed9e6f9847b357c
gala1234 <calero.vg@gmail.com>
2018-01-15 10:20:44 +0100

    correct active menu issue

--- js/hero.js
+++ js/hero.js
@@ -17,3 +17,24 @@ function starAnimation() {
 
 // Magic
 setTimeout(pageLoaded, 200);
+
+
+//menu, cambiar class active
+
+var navItem = document.querySelectorAll(".nav-item")
+// Functions
+
+function removeActiveClass() {
+	for(var i=0; i<navItem.length; i++) {
+	navItem[i].classList.remove('active');
+}
+};
+
+function addActiveClass(event) {
+	removeActiveClass();
+	event.currentTarget.classList.add('active');
+}
+
+for(var i=0; i<navItem.length; i++) {
+	navItem[i].addEventListener('click', addActiveClass);
+}
